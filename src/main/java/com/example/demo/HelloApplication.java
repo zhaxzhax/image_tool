7250e9659f33b4e3851e6f1d5add02970c5c0375
zhaxzhax <44116759+zhaxzhax@users.noreply.github.com>
2023-03-06 19:46:06 -0800

    init fxml

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.kordamp.bootstrapfx.BootstrapFX;
 
 import java.io.IOException;
 
@@ -11,8 +12,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 1368, 768);
+        scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
+        stage.setTitle("image tool");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,75 @@
 package com.example.demo;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+
+import java.io.*;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Button openButton;
+    @FXML
+    private ImageView imageView;
+    @FXML
+    private Label propertyText;
+    @FXML
+    private RadioButton jpg;
+    @FXML
+    private RadioButton png;
+    @FXML
+    private RadioButton gif;
+    @FXML
+    private RadioButton bmp;
+    @FXML
+    private RadioButton tiff;
+    @FXML
+    private ToggleGroup toggleGroup;
+    @FXML
+    private Button downloadButton;
+
+    private File selectedFile;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void handleOpenFile(ActionEvent event) {
+        // Create fileChooser
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Please choose the file");
+
+        // Create file filter
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Image Files",
+                "*.jpg", "*.jpeg",
+                "*.png",
+                "*.gif",
+                "*.bmp", "*.dib",
+                "*.tiff", "*.tif");
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        // display dialog
+        selectedFile = fileChooser.showOpenDialog(openButton.getScene().getWindow());
+
+        if (selectedFile != null) {
+            // Read image from path
+            try {
+                Image image = new Image(selectedFile.getPath());
+                String text = "Height: "+image.getHeight()+ '\n'
+                        +"Width: "+image.getWidth()+'\n'
+                        +"Location: "+image.getUrl()+'\n';
+                propertyText.setText(text);
+                imageView.setImage(image);
+            } catch (Exception e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setContentText("Failed to load image: " + e.getMessage());
+                alert.showAndWait();
+            }
+        }
+    }
+
+    @FXML
+    protected void handleDownloadFile(ActionEvent event){
+        System.out.println(toggleGroup.getSelectedToggle().toString());
     }
 }
